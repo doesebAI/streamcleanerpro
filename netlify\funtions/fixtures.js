c5c009b8b6104657482f6f48c2f5e48c419b2869
doesebAI <doesebprince96@gmail.com>
2025-08-28 21:38:53 +0200

    Delete netlify\funtions directory

--- netlify\funtions/fixtures.js
+++
@@ -1,7 +0,0 @@
-// netlify/functions/fixtures.js
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from CommonJS!" })
-  };
-};
